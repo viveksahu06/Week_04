b2ed413e066ef44a38d4ddf3bd7515878de22cb7
viveksahu06 <viveksahu1006@gmail.com>
2025-02-11 11:42:18 +0530

    [Vivek] Added Gadget class for Dynamic Online MarketPlace

---
+++ Day_01/src/main/java/com/generics/dynamiconlinemarketplace/Gadget.java
@@ -0,0 +1,23 @@
+/*2. Dynamic Online Marketplace
+Concepts: Type Parameters, Generic Methods, Bounded Type Parameters
+Problem Statement:
+Build a generic product catalog for an online marketplace that supports various product types like Books, Clothing, and Gadgets. Each product type has a specific price range and category.
+Hints:
+Define a generic class Product<T> where T is restricted to a category (BookCategory, ClothingCategory, etc.).
+Implement a generic method to apply discounts dynamically (<T extends Product> void applyDiscount(T product, double percentage)).
+Ensure type safety while allowing multiple product categories to exist in the same catalog.
+*/
+package com.generics.dynamiconlinemarketplace;
+
+// Gadgets category product
+class Gadget extends Product {
+    public Gadget(String name, double price) {
+        super(name, price);
+    }
+
+    @Override
+    public void displayProduct() {
+        System.out.println("Gadget: " + getName() + " | Price: $" + getPrice());
+    }
+}
+
