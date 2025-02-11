fd28c6cfebcef6cb75d41d66088ccbc8f5bd4c15
viveksahu06 <viveksahu1006@gmail.com>
2025-02-11 11:50:28 +0530

    [Vivek] Added VeganMeal Class for PersonalizedMealPlanGenerator

---
+++ Day_01/src/main/java/com/generics/mealplangenerator/VeganMeal.java
@@ -0,0 +1,29 @@
+/*4. Personalized Meal Plan Generator
+Concepts: Generic Methods, Type Parameters, Bounded Type Parameters
+Problem Statement:
+Design a Personalized Meal Plan Generator where users can choose different meal categories like Vegetarian, Vegan, Keto, or High-Protein. The system should ensure only valid meal plans are generated.
+Hints:
+Define an interface MealPlan with subtypes (VegetarianMeal, VeganMeal, etc.).
+Implement a generic class Meal<T extends MealPlan> to handle different meal plans.
+Use a generic method to validate and generate a personalized meal plan dynamically.*/
+package com.generics.mealplangenerator;
+
+// Vegan Meal
+class VeganMeal implements MealPlan {
+    private String mealName;
+
+    public VeganMeal(String mealName) {
+        this.mealName = mealName;
+    }
+
+    @Override
+    public String getMealName() {
+        return mealName;
+    }
+
+    @Override
+    public void displayMeal() {
+        System.out.println("Vegan Meal: " + mealName);
+    }
+}
+
