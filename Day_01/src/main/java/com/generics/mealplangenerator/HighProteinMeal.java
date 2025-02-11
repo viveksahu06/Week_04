64cc98ad8867bf0482ef00de99075c546cc17172
viveksahu06 <viveksahu1006@gmail.com>
2025-02-11 11:48:44 +0530

    [Vivek] Added HighProteinMeal class for PersonalizedMealPlanGenerator

---
+++ Day_01/src/main/java/com/generics/mealplangenerator/HighProteinMeal.java
@@ -0,0 +1,30 @@
+/*4. Personalized Meal Plan Generator
+Concepts: Generic Methods, Type Parameters, Bounded Type Parameters
+Problem Statement:
+Design a Personalized Meal Plan Generator where users can choose different meal categories like Vegetarian, Vegan, Keto, or High-Protein. The system should ensure only valid meal plans are generated.
+Hints:
+Define an interface MealPlan with subtypes (VegetarianMeal, VeganMeal, etc.).
+Implement a generic class Meal<T extends MealPlan> to handle different meal plans.
+Use a generic method to validate and generate a personalized meal plan dynamically.*/
+package com.generics.mealplangenerator;
+
+
+// High-Protein Meal
+class HighProteinMeal implements MealPlan {
+    private String mealName;
+
+    public HighProteinMeal(String mealName) {
+        this.mealName = mealName;
+    }
+
+    @Override
+    public String getMealName() {
+        return mealName;
+    }
+
+    @Override
+    public void displayMeal() {
+        System.out.println("High-Protein Meal: " + mealName);
+    }
+}
+
