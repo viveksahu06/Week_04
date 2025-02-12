7a6fb17cd6ddb1f5d701a7ce7737b8c0e544a6ab
viveksahu06 <viveksahu1006@gmail.com>
2025-02-12 22:40:47 +0530

    [Vivek] Added BankAccount class to store

---
+++ Day_02/src/main/java/org/collectionframwork/bankingsystem/BankAccount.java
@@ -0,0 +1,38 @@
+package org.collectionframwork.bankingsystem;
+
+class BankAccount {
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Account: " + accountNumber + ", Balance: " + balance;
+    }
+}
