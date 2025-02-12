593dcde596c82a4ddee4250a87aac9523e0875c3
viveksahu06 <viveksahu1006@gmail.com>
2025-02-12 10:11:54 +0530

    [Vivek] Added  class to store patient data

---
+++ Day_02/src/main/java/org/collectionframwork/queueinterface/hospitaltriagesystem/Patient.java
@@ -0,0 +1,11 @@
+package org.collectionframwork.queueinterface.hospitaltriagesystem;
+
+class Patient {
+    String name;
+    int severity;
+
+    public Patient(String name, int severity) {
+        this.name = name;
+        this.severity = severity;
+    }
+}
