b2954ab68ee302a4e773aaad4ed1fd96f4dd1c9f
viveksahu06 <viveksahu1006@gmail.com>
2025-02-12 15:53:02 +0530

    [Vivek] Fixed logic issue with Main class

--- Day_02/src/main/java/org/collectionframwork/queueinterface/hospitaltriagesystem/Main.java
+++ Day_02/src/main/java/org/collectionframwork/queueinterface/hospitaltriagesystem/Main.java
@@ -12,6 +12,6 @@ public class Main {
         // Processing patients
 
         System.out.println("Order of treatment:");
-        triage.processPatients();
+        System.out.println(triage.processPatients());
     }
 }
