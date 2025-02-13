23f4244219bf4ea15ccd98c536676963405819c7
viveksahu06 <viveksahu1006@gmail.com>
2025-02-13 16:54:59 +0530

    [Vivek] Added Main Data Streams - Store and Retrieve Primitive Data

---
+++ Day_03/src/main/java/com/streams/filehandling/storeandretrieveprimitivedata/Main.java
@@ -0,0 +1,9 @@
+package com.streams.filehandling.storeandretrieveprimitivedata;
+
+public class Main {
+    public static void main(String[] args) {
+        DataStreams.storeStudentDetails();
+
+        DataStreams.retrieveStudentDetails();
+    }
+}
