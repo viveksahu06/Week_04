79dff33267e5f13a8fe45273a04fbad5a04eaa9b
VIVEK KUMAR SAHU <113853773+viveksahu06@users.noreply.github.com>
2025-02-14 22:26:02 +0530

    Delete Day_04/src/main/java/com/exceptionhandling directory

--- Day_04/src/main/java/com/exceptionhandling/customexception/UserDefinedException.java
+++
@@ -1,9 +0,0 @@
-package com.exceptionhandling.customexception;
-
-//Creating a Custom Exception - Extending UserDefinedException to Exception
-class UserDefinedException extends Exception {
-    //Constructor
-    public UserDefinedException(String message) {
-        super(message);
-    }
-}
