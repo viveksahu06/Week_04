2ef20068898f620ced49bd96aa90ee26cd909da3
viveksahu06 <viveksahu1006@gmail.com>
2025-02-15 23:27:08 +0530

    [Vivek] Added Testing Parameterized Tests

---
+++ Day_05/src/main/java/com/problemstatement/junit/basic/testingparameterizedtests/NumberUtils.java
@@ -0,0 +1,9 @@
+package com.problemstatement.junit.basic.testingparameterizedtests;
+
+public class NumberUtils {
+
+    //Checking if number is even
+    public boolean isEven(int nums){
+        return nums % 2 ==0;
+    }
+}
