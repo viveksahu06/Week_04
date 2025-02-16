3c2198e49a47156b0307050138f4b012d693228b
viveksahu06 <viveksahu1006@gmail.com>
2025-02-16 23:03:49 +0530

    [Vivek] Added Get Class Information

---
+++ Day_06/src/main/java/com/reflection/basiclevel/CreateObjectReflection.java
@@ -0,0 +1,35 @@
+package com.reflection.basiclevel;
+
+import java.lang.reflect.*;
+
+class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public void display() {
+        System.out.println("Student Name: " + name);
+    }
+}
+
+public class CreateObjectReflection {
+    public static void main(String[] args) {
+        try {
+            // Getting Class object
+            Class<?> clazz = Student.class;
+
+            // Accessing constructor
+            Constructor<?> constructor = clazz.getConstructor(String.class);
+
+            // Creating instance dynamically
+            Student student = (Student) constructor.newInstance("Alice");
+
+            // Calling method
+            student.display();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
